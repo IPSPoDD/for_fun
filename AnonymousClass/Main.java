fda8bab571894b593bfc28d2643bfdc6e99ca5d6
IPSPoDD <57214472+IPSPoDD@users.noreply.github.com>
2020-05-07 20:58:38 +0300

    Create Main.java

---
+++ AnonymousClass/Main.java
@@ -0,0 +1,15 @@
+package itproger.JavaApp_v1;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        new Computer(){
+          void superComp(){
+              this.i7.start();
+              this.transfer.start();
+          }
+        };
+        }
+    }
